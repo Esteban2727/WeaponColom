c5ba79f9657d20c0aa629af2279630ece853a5e8
estebanCastro <escastr@gmail.com>
2024-04-13 17:06:16 -0500

    adecuando los @Query para adecuaderse a celularesy pcs

---
+++ WeaponColom/src/App.jsx
@@ -0,0 +1,17 @@
+import Mainpage from "./MainPage"
+
+function App() {
+
+
+  return (
+    <>
+    
+    <div className="Container_main">
+    <Mainpage/>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ WeaponColom/src/MainPage.jsx
@@ -0,0 +1,9 @@
+import "./MainPage.css"
+export default function Mainpage(){
+    return (
+    
+    <div className="Container_Header">
+    <p className="Container_Header__editTitle">WEAPON COLOM </p>   
+    </div>
+    )
+}
