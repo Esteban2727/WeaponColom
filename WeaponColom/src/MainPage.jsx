d6f8ce020fa893e1a0326643cdd43b85ce272f98
estebanCastro <escastr@gmail.com>
2024-04-13 17:43:18 -0500

    create icons for buying,sign up and sign in

--- WeaponColom/src/MainPage.jsx
+++ WeaponColom/src/MainPage.jsx
@@ -1,9 +1,18 @@
 import "./MainPage.css"
+import { BsCart4 } from "react-icons/bs"
+import { BsFillPersonPlusFill } from "react-icons/bs";
+import { BsPersonRaisedHand } from "react-icons/bs";
 export default function Mainpage(){
     return (
     
     <div className="Container_Header">
-    <p className="Container_Header__editTitle">WEAPON COLOM </p>   
+    <p className="Container_Header__editTitle">WEAPON COLOM </p> 
+    <div className="Container_Header__modals"> 
+    <BsCart4  className="Container_Header__buycar"/>  
+    <BsFillPersonPlusFill className="Container_Header__buycar"  />
+    <BsPersonRaisedHand className="Container_Header__buycar"/>
+    </div>
+    
     </div>
     )
 }
