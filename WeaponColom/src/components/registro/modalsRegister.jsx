ec7449968945abaed7404e2853363c37ac31fc4f
estebanCastro <escastr@gmail.com>
2024-04-14 16:56:44 -0500

    prueba conectar frontend con backend

--- WeaponColom/src/components/registro/modalsRegister.jsx
+++ WeaponColom/src/components/registro/modalsRegister.jsx
@@ -3,7 +3,25 @@ import Modal from 'react-bootstrap/Modal';
 
 import { useState } from 'react';
 export default function ModalsRegister({show1,handleClose1}){
-
+  const [Getdatas,setGetdatas]= useState([])
+  const GetData =async(data)=>{
+      const url=`https://localhost:3000`
+     const save= await fetch(url,{
+      method: "Post",
+      headers:{
+        "Conten-Type" : "application/json"
+      },
+      Body : JSON.stringify({
+        usuario
+      })
+     })
+     const convertir= await save.json()
+     setGetdatas(convertir)
+  const [usuario,SetUsuario]=useState("")
+   function email(e){
+   const save= e.target.value
+    SetUsuario(save)
+   }
 
     return <Modal show={show1} onHide={handleClose1}>
         <div className='container_edit'>
@@ -16,60 +34,10 @@ export default function ModalsRegister({show1,handleClose1}){
       <div className='input'>
       <input type='text' className='name' placeholder='Nombre' ></input>
       <input type="text"  className='name' placeholder='Apellido'></input>
-      <input type="number" className='name1' placeholder='Número de celular o correo electrónico'></input>
+      <input type="email" className='name1' placeholder='Número de celular o correo electrónico' onChange={e=>{email(e)}}></input>
       <input  type="password"className='name1' placeholder='Contraseña nueva'></input>
+      <Button onClick={()=>{GetData()}} >REGISTRARSE</Button>
       
-      <div className='fecha'>
-      <select className='component_input_day'>
-      <option value={1}>1</option>
-      <option value={2}>2</option>
-      <option value={3}>3</option>
-      <option value={4}>4</option>
-      <option value={5}>5</option>
-      <option value={6}>6</option>
-      <option value={7}>7</option>
-      <option value={8}>8</option>
-      <option value={9}>9</option>
-      <option value={10}>10</option>
-      <option value={11}>11</option>
-      <option value={12}>12</option>
-      <option value={13}>13</option>
-      <option value={14}>14</option>
-      <option value={15}>15</option>
-      <option value={16}>16</option>
-      <option value={17}>17</option>
-      <option value={18}>18</option>
-      <option value={19}>19</option>
-      <option value={20}>20</option>
-      <option value={21}>21</option>
-      <option value={22}>22</option>
-      <option value={23}>23</option>
-      <option value={24}>24</option>
-      <option value={25}>25</option>
-      <option value={26}>26</option>
-      <option value={27}>27</option>
-      <option value={28}>28</option>
-      <option value={29}>29</option>
-      <option value={30}>30</option>
-      <option value={31}>31</option>
- 
-      </select>
-      <select className='component_input_day'>
-      <option value={1}>enero</option>
-      <option value={2}>febrero</option>
-      <option value={3}>marzo</option>
-      <option value={4}>abril</option>
-      <option value={5}>mayo</option>
-      <option value={6}>junio</option>
-      <option value={7}>julio</option>
-      <option value={8}>agosto</option>
-      <option value={9}>septiembre</option>
-      <option value={10}>octubre</option>
-      <option value={11}>noviembre</option>
-      <option value={12}>diciembre</option>
-      </select>
-      </div>
-
       </div>
       </div>
     
