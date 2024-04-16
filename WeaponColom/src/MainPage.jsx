58fcf010fa174c86ddf0ba6e7fab67ca4c0fae0c
estebanCastro <escastr@gmail.com>
2024-04-15 22:37:20 -0500

    add the routes

--- WeaponColom/src/MainPage.jsx
+++ WeaponColom/src/MainPage.jsx
@@ -7,6 +7,12 @@ import { useState } from 'react';
 import Modals from "./components/carrito/modals";
 import ModalsRegister from "./components/registro/modalsRegister";
 import ModalsSignIn from "./components/sing in/modasSignin";
+
+import escopeta from "../src/assets/escopeta.png"
+import franco from "../src/assets/franco.png"
+import fusil from "../src/assets/fusil.png"
+import pistola from "../src/assets/pistola.png"
+
 export default function Mainpage(){
     const [show, setShow] = useState(false);
     const [show1, setShow1] = useState(false);
@@ -22,7 +28,7 @@ export default function Mainpage(){
     const handleClose2 = () => setShow2(false);
     const handleShow2 = () => setShow2(true);
     return (
-    
+    <>
     <div className="Container_Header">
     <img className="Container_header__imageLOGO  " src={image} alt="image logo" style={{ width: '90px', height: '70px' }} ></img>
     <p className="Container_Header__editTitle">WEAPON COLOM </p> 
@@ -34,7 +40,15 @@ export default function Mainpage(){
     <ModalsRegister show1={show1} handleClose1={handleClose1}/>
     <ModalsSignIn show2={show2} handleClose2={handleClose2}/>
     </div>
+
     
     </div>
+    <div className="redireccion">
+    <button className="edit_categorias" onClick={()=>{window.location.href = "/pistola"}}> <img src={pistola} className="edit_image_categoria" alt="iamge weapons"></img></button>
+        <button className="edit_categorias" onClick={()=>{window.location.href = "/escopeta"}} ><img src={escopeta} className="edit_image_categoria" alt="iamge weapons"></img></button>
+        <button className="edit_categorias" onClick={()=>{window.location.href = "/fusil"}} ><img src={fusil}  className="edit_image_categoria" alt="iamge weapons"></img></button>
+        <button className="edit_categorias" onClick={()=>{window.location.href = "/franco"}} ><img src={franco} className="edit_image_categoria" alt="iamge weapons"></img></button>
+        </div>  
+    </>
     )
 }
